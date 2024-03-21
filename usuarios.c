60a60f1dc15c66f8dc6c88a4b7cd3c5ee6ca9598
Mariio711 <marioevpacheco@gmail.com>
2024-03-21 11:06:28 +0100

    añadido usuarios h

---
+++ usuarios.c
@@ -0,0 +1,10 @@
+//en este modulo se encuentra la funcion que se encarga de gestionar los usuarios, cargar los txt en los struct y la autenticacion del usuario
+
+#include <stdio.h>
+#include <string.h>
+#include "usuarios.h"
+
+void main(){
+    cargar_datos();
+    login();
+}
