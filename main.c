7d0634da27c57d6961c4eb418a761dcd9336d0fc
Mariio711 <marioevpacheco@gmail.com>
2024-03-21 10:56:55 +0100

    crados main y suaurios

---
+++ main.c
@@ -0,0 +1,6 @@
+// de aqui partira el programa, necesitamos llamar a la funcion para cargar los datos de los txt a las structs y abrir el login
+
+#include <stdio.h>
+#include <string.h>
+#include "transportista.h"
+
