e45c6f00184253a15836cfb30c3ed97ec59def09
Antonio7225 <Abaddonruiz@gmail.com>
2024-04-04 11:46:05 +0200

    clientes.c terminado(falta ficheros)

--- Clientes.h
+++ Clientes.h
@@ -12,12 +12,29 @@ typedef struct{
     float dinero;
 }cliente_estr;
 
+typedef struct{
+    int id;
+    char id_descuento[10];
+    char fecha_inicial[10];
+    char fecha_limite[10];
+    int estado;
+}desclient_estr;
+
+typedef struct{
+    char id_descuento[10];
+    char descripcion[50];
+    char tipo[6];
+    int estado;
+    float importe;
+    char aplicabilidad[10];
+}descuentos_estr;
+
 void ficheros(int,cliente_estr *);
 void bienvenida(cliente_estr *);
 
 void perfil(cliente_estr *);
 void productos();
-void descuentos();
+void descuentos(cliente_estr *);
 void pedidos();
 void devolucion();
 
