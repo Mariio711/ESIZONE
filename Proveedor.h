f5b29ed912016457a7a77a83ace692d60f77280e
Mariio711 <80849476+Mariio711@users.noreply.github.com>
2024-04-23 18:25:56 +0200

    quitado struct duplicada

--- Proveedor.h
+++ Proveedor.h
@@ -1,14 +1,6 @@
 #ifndef PROVEEDOR_H
 #define PROVEEDOR_H
 
-typedef struct{
-    int id_empresa;
-    char nombre_empresa[20];
-    char correo[30];
-    char clave[15];
-    char perfil_usuario[13];
-}usuarios;
-
 //void inicio_prov();
 void bienvenida_prov(usuarios *,int);
 void perfil_prov(usuarios *,int);
