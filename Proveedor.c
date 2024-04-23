70d8b91cecb78a9000bce7ae6683031351927221
Antonio7225 <Abaddonruiz@gmail.com>
2024-04-23 19:12:26 +0200

    provedor arreglado

--- Proveedor.c
+++ Proveedor.c
@@ -30,7 +30,7 @@ void bienvenida_prov(usuarios *provedor,int id_prov){
     do{
         system("cls");
 
-        printf("Empresa: %s\nQue desea hacer?\n1. Perfil\n2. Productos\n3. Pedidos\n4. Salir <-\n",(provedor+id_prov)->nombre_empresa);
+        printf("Empresa: %s\nQue desea hacer?\n1. Perfil\n2. Productos\n3. Pedidos\n4. Salir <-\n",(provedor+id_prov)->Nombre);
         do{
             scanf("%i",&elec_b);
             if(elec_b<1||elec_b>6)
@@ -58,8 +58,8 @@ void perfil_prov(usuarios *provedor,int id_prov){
     int elec_perfil;
     do{
         system("cls");
-        printf("empresa: %s\n",(provedor+id_prov)->nombre_empresa);
-        printf("correo: %s\n",(provedor+id_prov)->correo);
+        printf("empresa: %s\n",(provedor+id_prov)->Nombre);
+        printf("correo: %s\n",(provedor+id_prov)->email);
         printf("-----------------------------------------------------\n");
         printf("Que quieres hacer?\n1. Modificar nombre de la empresa\n2. Modificar correo\n3. Modificar clave\n4. volver <-\n");
 
@@ -88,17 +88,17 @@ void mod_nom_prov(usuarios *provedor,int id_prov){
     char nombre_introducido[20];
     char clave_introducida[15];
     system("cls");
-    printf("el nombre de la empresa actual es: %s\nintroduce tu nuevo nombre:",(provedor+id_prov)->nombre_empresa);
+    printf("el nombre de la empresa actual es: %s\nintroduce tu nuevo nombre:",(provedor+id_prov)->Nombre);
     gets(nombre_introducido);
     fflush(stdin);
 
     printf("introduzca su clave para confirmar:");
     gets(clave_introducida);
     fflush(stdin);
-        if(strcmp((provedor+id_prov)->clave,clave_introducida)==0){
+        if(strcmp((provedor+id_prov)->Contrasena,clave_introducida)==0){
             printf("Clave correcta :)\n");                          
             system("pause");
-            strcpy((provedor+id_prov)->nombre_empresa,nombre_introducido);
+            strcpy((provedor+id_prov)->Nombre,nombre_introducido);
         }
         else{
             printf("Clave incorrecta :(\n");
@@ -113,17 +113,17 @@ void mod_email_prov(usuarios *provedor,int id_prov){
     char email_introducido[30];
     char clave_introducida[15];
     system("cls");
-    printf("Tu correo actual es: %s\nintroduce tu nuevo correo:",(provedor+id_prov)->correo);
+    printf("Tu correo actual es: %s\nintroduce tu nuevo correo:",(provedor+id_prov)->email);
     gets(email_introducido);
     fflush(stdin);
 
     printf("introduzca su clave para confirmar:");
     gets(clave_introducida);
     fflush(stdin);
-        if(strcmp((provedor+id_prov)->clave,clave_introducida)==0){
+        if(strcmp((provedor+id_prov)->Contrasena,clave_introducida)==0){
             printf("Clave correcta :)\n");                          
             system("pause");
-            strcpy((provedor+id_prov)->correo,email_introducido);
+            strcpy((provedor+id_prov)->email,email_introducido);
         }
         else{
             printf("Clave incorrecta :(\n");
@@ -142,7 +142,7 @@ void mod_contr_prov(usuarios *provedor,int id_prov){
     printf("introduzca su antigua clave para confirmar:");
     gets(clave_introducida);
     fflush(stdin);
-        if(strcmp((provedor+id_prov)->clave,clave_introducida)==0){
+        if(strcmp((provedor+id_prov)->Contrasena,clave_introducida)==0){
             printf("Clave correcta :)\n");                          
             system("pause");
             do{
@@ -155,7 +155,7 @@ void mod_contr_prov(usuarios *provedor,int id_prov){
                 if(strcmp(clave_nueva,confirmar)!=0)
                     printf("\nlas claves no coinciden :(\n");
             }while(strcmp(clave_nueva,confirmar)!=0);
-            strcpy((provedor+id_prov)->clave,clave_nueva);
+            strcpy((provedor+id_prov)->Contrasena,clave_nueva);
         }
         else{
             printf("Clave incorrecta :(\n");
@@ -255,11 +255,11 @@ void descarga_prov(usuarios *provedor,int n){
                 j=-1;
             }
             if(c=='\n'){
-                strcpy((provedor+i)->nombre_empresa,nombre_empresa);
-                strcpy((provedor+i)->correo,correo);
-                strcpy((provedor+i)->clave,clave);
-                strcpy((provedor+i)->perfil_usuario,perfil_usuario);
-                (provedor+i)->id_empresa=atoi(id_prov);
+                strcpy((provedor+i)->Nombre,nombre_empresa);
+                strcpy((provedor+i)->email,correo);
+                strcpy((provedor+i)->Contrasena,clave);
+                strcpy((provedor+i)->Perfil_usuario,perfil_usuario);
+                strcpy((provedor+i)->Id_empresa,id_prov);
                 num_guion=0;
                 j=-1;
             }
@@ -296,11 +296,11 @@ void carga_prov(usuarios *provedor,int n){
     }
     else{
         do{
-            fprintf(archivo,"%i-",(provedor+i)->id_empresa);
-            fprintf(archivo,"%s-",(provedor+i)->nombre_empresa);
-            fprintf(archivo,"%s-",(provedor+i)->correo);
-            fprintf(archivo,"%s-",(provedor+i)->clave);
-            fprintf(archivo,"%s\n",(provedor+i)->perfil_usuario);
+            fprintf(archivo,"%s-",(provedor+i)->Id_empresa);
+            fprintf(archivo,"%s-",(provedor+i)->Nombre);
+            fprintf(archivo,"%s-",(provedor+i)->email);
+            fprintf(archivo,"%s-",(provedor+i)->Contrasena);
+            fprintf(archivo,"%s\n",(provedor+i)->Perfil_usuario);
             i++;
         }while(i!=n-1);
     }
