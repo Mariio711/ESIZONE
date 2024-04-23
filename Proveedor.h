a36b1df4e665e78bd45467822272feea2b8c8d99
Antonio7225 <Abaddonruiz@gmail.com>
2024-04-23 18:19:16 +0200

    provedor terminado y en dinamico

--- Proveedor.h
+++ Proveedor.h
@@ -10,15 +10,15 @@ typedef struct{
 }usuarios;
 
 //void inicio_prov();
-void bienvenida_prov(usuarios [],int);
-void perfil_prov(usuarios [],int);
+void bienvenida_prov(usuarios *,int);
+void perfil_prov(usuarios *,int);
 
-void mod_nom_prov(usuarios [],int);
-void mod_email_prov(usuarios [],int);
-void mod_contr_prov(usuarios [],int);
+void mod_nom_prov(usuarios *,int);
+void mod_email_prov(usuarios *,int);
+void mod_contr_prov(usuarios *,int);
 
-void carga_prov(usuarios [],int);
-void descarga_prov(usuarios [],int);
+void carga_prov(usuarios *,int);
+void descarga_prov(usuarios *,int);
 int num_prov();
 
 #endif
